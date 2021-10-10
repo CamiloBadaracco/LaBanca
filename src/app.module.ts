05c709818315cc39d91b0cfc2e3f12b79195f487
santibeguiristain <santibeguiristain1992@@outlook.es>
2021-10-10 11:58:35 -0900

    User
     Domain
     infrastructure
     use-cases

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AgentModule } from './agent/infrastructure/modules/agent.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import 'reflect-metadata';
+import { UserModule } from './user/infrastructure/modules/user.module';
 //import { MailModule } from './mail/infrastructure/modules/mail.module';
 
 @Module({
@@ -11,6 +12,7 @@ import 'reflect-metadata';
     TypeOrmModule.forRoot(typeOrmConfig),
     ConfigModule.forRoot(),
     AgentModule,
+    UserModule,
    // MailModule,
   ],
 })
