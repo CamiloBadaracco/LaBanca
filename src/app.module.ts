045df159bac391eede92853aa5913221b7ca203f
santibeguiristain <santibeguiristain1992@@outlook.es>
2021-10-10 13:20:06 -0900

    Addres  (Post / Get)
     Domain
     Infras
     Uses

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { AgentModule } from './agent/infrastructure/modules/agent.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import 'reflect-metadata';
 import { UserModule } from './user/infrastructure/modules/user.module';
+import { SubAgent } from './subAgent/domain/subAgent.entity';
+import { Address } from './address/domain/address.entity';
 //import { MailModule } from './mail/infrastructure/modules/mail.module';
 
 @Module({
@@ -13,6 +15,8 @@ import { UserModule } from './user/infrastructure/modules/user.module';
     ConfigModule.forRoot(),
     AgentModule,
     UserModule,
+    SubAgent,
+    Address
    // MailModule,
   ],
 })
